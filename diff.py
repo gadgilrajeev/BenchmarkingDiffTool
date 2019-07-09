b2347d9f2f3e7649bc33f10ec12f0751b402b567
Sumedh Patkar <sumedh.patkar19@gmail.com>
2019-07-08 22:50:13 -0700

    Created page for showing all HPC and Cloud tests

--- diff.py
+++ diff.py
@@ -1,177 +1,67 @@
-import os
+import pandas as pd
+import pymysql
 import configparser
-import csv
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, render_template, request
 app = Flask(__name__)
+import pprint
 
-@app.route('/',methods=['GET','POST'])
-def home():
-    if(request.method == 'GET'):
-        #print("\nGET\n");
-        return render_template('base.html',context={})
-    elif(request.method == 'POST'):
-        #print("\nPOST\n");
-        parser = configparser.ConfigParser()
+@app.route('/')
+def home_page():
+	parser = configparser.ConfigParser()
+	wiki_metadata_file_path = '/mnt/nas/scripts/wiki_description.ini'
+	parser.read(wiki_metadata_file_path)
 
-        test_numbers = []
+	hpc_benchmarks_list = []
+	cloud_benchmarks_list = []
+	print("###############################")
+	print("PRINTING SECTIONS AND MODELS")
+	print("###############################")
+	for section in parser.sections():
+		print(section + ":" + parser.get(section,'model'))
+		type_of_benchmark = parser.get(section,'model').strip()
+		print(type(type_of_benchmark))
+		if(type_of_benchmark == '\"hpc\"'):
+			hpc_benchmarks_list.append(section)
+		else:
+			cloud_benchmarks_list.append(section)
 
-        #get all the test numbers passed from the form
-        while (True):
-            if (request.form.get('test_' + str(len(test_numbers)+1))):
-                test_numbers.append(request.form.get('test_' + str(len(test_numbers)+1)))
-            else:
-                break
+	print(hpc_benchmarks_list)
+	print("\n\n\n")
+	print(cloud_benchmarks_list)
 
-        #just for debugging stuff
-        #print(test_numbers)
+	hpc_benchmarks_list = sorted(hpc_benchmarks_list,key=str.lower)
+	cloud_benchmarks_list = sorted(cloud_benchmarks_list,key=str.lower)
+	context = {
+	'hpc_benchmarks_list':hpc_benchmarks_list,
+	'cloud_benchmarks_list':cloud_benchmarks_list,
+	}
+	return render_template('all-tests.html',context=context)
 
-        #PERFORM EXCEPTION HANDLING HERE AND PROCESS ACCORDINGLY
-        try:
-            #check if test number exists
-            for t in test_numbers:
-                if not os.path.isdir('../tests/test_'+t):
-                    raise Exception("Test number " + t + " does not exist yet")
+@app.route('/allruns/<testname>')
+def showAllRuns(testname):
+	query = "SELECT DISTINCT o.originID, o.testdate, o.notes from origin o INNER JOIN result r ON o.originID = r.origin_originID INNER JOIN testdescriptor t ON t.testdescriptorID = o.testdescriptor_testdescriptorID where t.testname = \'" + testname + "\' AND r.isvalid = 1 ORDER BY o.originID DESC" 
+	db = pymysql.connect(host = '10.110.169.149', user = 'root', passwd = '', db = 'benchtooldb', port = 3306)
 
-            #check for duplicate entries
-            if( len(test_numbers) > len(list(dict.fromkeys(test_numbers)))):
-                raise Exception("Please enter distinct values")
-        except Exception as error_message:
-            error = {
-                'message': error_message,
-            }
-            #render the page with error messages
-            return render_template('base.html', error=error, context={})
+	dataframe = pd.read_sql(query, db)
+	rows,columns = dataframe.shape		#returns a tuple (rows,columns)
 
-        #IF everything OK proceed further
-        #retrieve and read metadata.ini file from ../tests directory
-        parser.read('../tests/metadata.ini')
+	context = {
+	'testname':testname,
+	'data' : dataframe.to_dict(orient='list'),
+	'no_of_rows': rows,
+	'no_of_columns': columns,
+	'direct_data':dataframe.to_html(index=False),
+	}
 
-        #create all parameter lists
-        bootenv_param_list = parser.get('bootenv', 'db_variables').replace(' ','').split(',')
-        node_param_list = parser.get('node', 'db_variables').replace(' ','').split(',')
-        hwdetails_param_list = parser.get('hwdetails', 'db_variables').replace(' ','').split(',')
-        ostunings_param_list = parser.get('ostunings', 'db_variables').replace(' ', '').split(',')
-        toolchain_param_list = parser.get('toolchain', 'db_variables').replace(' ', '').split(',')
-        testdescriptor_param_list = parser.get('testdescriptor', 'db_variables').replace(' ', '').split(',')
-        jenkins_param_list = parser.get('jenkins', 'db_variables').replace(' ', '').split(',')
-        origin_param_list = parser.get('origin', 'db_variables').replace(' ', '').split(',')
+	return render_template('all-runs.html', context = context)
 
-        #LIST of Dictionaries (tests) to be compared
-        bootenv_list = []
-        node_list = []
-        hwdetails_list = []
-        ostunings_list = []
-        toolchain_list = []
-        testdescriptor_list = []
-        jenkins_list = []
-        origin_list = []
+@app.route('/test-details/<originID>')
+def showTestDetails(originID):
+	query = ""
+	db = pymysql.connect(host = '10.110.169.149', user = 'root', passwd = '', db = 'benchtooldb', port = 3306)
 
-        #retrieve the metadata for EACH .csv FILE using the csv module
-        #FOR BOOTENV
-        for t_no in test_numbers:
-            bootenv = dict()
-            with open('../tests/test_' + t_no +'/bootenv.csv', newline='') as csvfile:
-                reader = csv.reader(csvfile, delimiter=',')
-                for row in reader:
-                    for i in range(0, len(bootenv_param_list)):
-                        bootenv[bootenv_param_list[i]] = row[i]
-            bootenv_list.append(bootenv)
+	df = pd.read_sql(query, db)
 
-        #FOR NODE
-        for t_no in test_numbers:
-            node = dict()
-            with open('../tests/test_' + t_no + '/node.csv', newline='') as csvfile:
-                reader = csv.reader(csvfile, delimiter=',')
-                for row in reader:
-                    for i in range(0, len(node_param_list)):
-                        node[node_param_list[i]] = row[i]
-            node_list.append(node)
-
-        # FOR HWDETAILS
-        for t_no in test_numbers:
-            hwdetail = dict()
-            with open('../tests/test_' + t_no + '/hwdetails.csv', newline='') as csvfile:
-                reader = csv.reader(csvfile, delimiter=',')
-                for row in reader:
-                    for i in range(0, len(hwdetails_param_list)):
-                        hwdetail[hwdetails_param_list[i]] = row[i]
-                    break
-            hwdetails_list.append(hwdetail)
-
-        # FOR OSTUNINGS
-        for t_no in test_numbers:
-            ostuning = dict()
-            with open('../tests/test_' + t_no + '/ostunings.csv', newline='') as csvfile:
-                reader = csv.reader(csvfile, delimiter=',')
-                for row in reader:
-                    for i in range(0, len(ostunings_param_list)):
-                        ostuning[ostunings_param_list[i]] = row[i]
-            ostunings_list.append(ostuning)
-
-        #FOR TOOLCHAIN
-        for t_no in test_numbers:
-            toolchain = dict()
-            with open('../tests/test_' + t_no + '/toolchain.csv', newline='') as csvfile:
-                reader = csv.reader(csvfile, delimiter=',')
-                for row in reader:
-                    for i in range(0, len(toolchain_param_list)):
-                        toolchain[toolchain_param_list[i]] = row[i]
-            toolchain_list.append(toolchain)
-
-        #FOR TESTDESCRIPTOR
-        for t_no in test_numbers:
-            testdescriptor = dict()
-            with open('../tests/test_' + t_no + '/testdescriptor.csv', newline='') as csvfile:
-                reader = csv.reader(csvfile, delimiter=',')
-                for row in reader:
-                    for i in range(0, len(testdescriptor_param_list)):
-                        testdescriptor[testdescriptor_param_list[i]] = row[i]
-            testdescriptor_list.append(testdescriptor)
-
-        #FOR JENKINS
-        for t_no in test_numbers:
-            jenkins = dict()
-            with open('../tests/test_' + t_no + '/jenkins.csv', newline='') as csvfile:
-                reader = csv.reader(csvfile, delimiter=',')
-                for row in reader:
-                    for i in range(0, len(jenkins_param_list)):
-                        jenkins[jenkins_param_list[i]] = row[i]
-            jenkins_list.append(jenkins)
-
-        #FOR origin
-        for t_no in test_numbers:
-            origin = dict()
-            with open('../tests/test_' + t_no + '/origin.csv', newline='') as csvfile:
-                reader = csv.reader(csvfile, delimiter=',')
-                for row in reader:
-                    for i in range(0, len(origin_param_list)):
-                        origin[origin_param_list[i]] = row[i]
-            origin_list.append(origin)
-
-        #create a dictionary 'context'. This is what will be displayed in the form of tables
-        context = {
-            'test_numbers': test_numbers,
-
-            'bootenv_param_list': bootenv_param_list,
-            'node_param_list': node_param_list,
-            'hwdetails_param_list': hwdetails_param_list,
-            'ostunings_param_list': ostunings_param_list,
-            'toolchain_param_list': toolchain_param_list,
-            'testdescriptor_param_list': testdescriptor_param_list,
-            'jenkins_param_list': jenkins_param_list,
-            'origin_param_list': origin_param_list,
-
-            'bootenvs': bootenv_list,
-            'nodes': node_list,
-            'hwdetails':hwdetails_list,
-            'ostunings':ostunings_list,
-            'toolchain':toolchain_list,
-            'testdescriptor':testdescriptor_list,
-            'jenkins':jenkins_list,
-            'origin':origin_list,
-
-        }
-
-        return render_template('base.html',context=context)
+@app.route('/environment-details/<originID>')
+def showEnvDetails(originID):
+	query = ""
